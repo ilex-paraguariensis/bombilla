e81ef72cff2f27277d139b048611cd3c06d5eef2
GiulioZani <giulio.zani@gmail.com>
2022-10-05 18:22:08 +0200

    minor stuff

--- packages/bombilla/bombilla.py
+++ packages/bombilla/bombilla.py
@@ -7,16 +7,16 @@ import ipdb
 class Bombilla:
     def __init__(
         self,
-        bambilla_dict: dict,
+        bombilla_dict: dict,
         root_module: str = "",
         base_module: str = "",
         object_key_map: dict = {},
     ) -> None:
 
-        assert isinstance(bambilla_dict, dict), "Bambilla must be a dict"
+        assert isinstance(bombilla_dict, dict), "Bambilla must be a dict"
 
         Node.set_config(base_module, root_module, object_key_map)
-        self.root_node = NodeDict(dict(bambilla_dict))
+        self.root_node = NodeDict(dict(bombilla_dict))
 
     def load(self):
         self.root_node.__load__()
@@ -31,7 +31,7 @@ class Bombilla:
         return self.root_node.generate_full_dict()
 
     def execute_method(
-        self, method_name: str, object_name: str = None, *args, **kwargs
+        self, method_name: str, object_name: str, *args, **kwargs
     ):
 
         self.root_node.find(object_name).call_method(method_name, *args, **kwargs)
